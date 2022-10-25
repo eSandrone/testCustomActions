42f3c5b3f0a8e5840069409160eaafd25a87d991
Emanuele Sandrone <107463865+eSandrone@users.noreply.github.com>
2022-10-25 13:08:52 +0200

    Add files via upload

---
+++ actions/readEnvFiles/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs')
+const dotenv = require('dotenv')
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const pathEnvFile = core.getInput('env-file-path') || '.';
+  fs.readFile(pathEnvFile, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    core.setOutput("data", data);
+    const variables = dotenv.parse(data);
+    Object.entries(variables).forEach(setEnvVar(key, value))
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+  })
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function setEnvVar(key, value) {
+  core.exportVariable(key, value);
+}
